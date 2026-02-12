2e41ee05e738bf5868b596fc84f0298457b8aee0
Chaya B R <chayabr31@gmail.com>
2026-02-12 09:36:19 +0530

    completed till the jagged

--- demo.java
+++ demo.java
@@ -13,3 +13,29 @@ class demo{
         }
     }
 }
+
+class demo2 {
+    public static void main(String[] args) {
+        String name = new String("Hello");
+        String name2 = new String("Hello");
+        System.out.println(name == name2);
+    }
+}
+
+ class Main {
+    public static void main(String[] args) {
+        //declare and initialize an array
+        int[] numbers = {5, -2, 0, 10, -7};
+        System.out.println("Elements of the array:");
+        for (int num : numbers) {
+            if (num > 0) {
+                System.out.println(num + " is positive");
+            } else if (num < 0) {
+                System.out.println(num + " is negative");
+            } else {
+                System.out.println(num + " is zero");
+            }
+        }
+    }
+}
+
